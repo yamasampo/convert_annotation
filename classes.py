f0786d449302c8594d0dae47e891d982750d34ba
Haruka Yamashita <>
2019-07-30 16:42:20 +0900

    Fix returns if query was not found.

--- classes.py
+++ classes.py
@@ -158,7 +158,7 @@ class ConvertCoordinates(Database):
         
         # If query is not found, return -9
         if len(conv_dicts) == 0:
-            return GenomicCoordinate('', -9, -9, -9)
+            return [GenomicCoordinate('', -9, -9, -9)]
         
         # Collect results
         results = []
