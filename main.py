fb8ba123e3323dd8e807467a38dfc6ad864e8fb1
Haruka Yamashita <hyamashita@nig.ac.jp>
2022-01-27 13:23:43 +0900

    Add docstring.

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 from . import read
 from . import formatter
 
-def main(map_csv_path, query_version, ref_version, 
-    query_path, out_csv_path, description=''):
+def main(
+        map_csv_path:str, 
+        query_version:str, 
+        ref_version:str, 
+        query_path:str, 
+        out_csv_path:str):
+    """ Outputs a file with converted coordinates from a given query file. 
+    
+    Parameters
+    ----------
+    map_csv_path: str
+        A path to a file including the corresponding coordinates. 
+    query_version: str, int or tuple
+        A key to keep track of query data.
+    ref_version: str, int or tuple
+        A key to keep track of reference data. 
+    query_path: str
+        A path to a file including that a user wishes to convert its cooredinate
+        to the reference version. 
+    out_csv_path: str
+        A path to an output file. 
+    
+    """
     cc = read.from_CSV_to_ConvertCoordinates(
         map_csv_path, query_version, ref_version, description='')
 
